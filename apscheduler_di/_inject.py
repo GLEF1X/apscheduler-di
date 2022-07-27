01ed1aa7e4027ce836deed81f573b8c2fd40ded1
GLEF1X <glebgar567@gmail.com>
2022-07-27 15:44:49 +0300

    :bug: fix #1

--- apscheduler_di/_inject.py
+++ apscheduler_di/_inject.py
@@ -13,14 +13,14 @@ from apscheduler_di._binding import normalize_job_executable
 from apscheduler_di._serialization import SharedJob
 
 
-def inject_dependencies_to_scheduler(scheduler: BaseScheduler, ctx: Container):
-    prepared_context = ctx.build_provider()
+def inject_dependencies_to_scheduler(scheduler: BaseScheduler, container: Container) -> None:
+    services = container.build_provider()
     for job_store in scheduler._jobstores.values():  # type: BaseJobStore  # noqa
 
-        def func_get_due_jobs_with_context(c: BaseJobStore, now: datetime):
-            jobs: List[Job] = type(job_store).get_due_jobs(c, now)
+        def func_get_due_jobs_with_context(c: BaseJobStore, now: datetime) -> List[Job]:
+            jobs: List[Job] = type(c).get_due_jobs(c, now)
             return _convert_raw_to_easy_maintainable_jobs(
-                scheduler, jobs, prepared_context
+                scheduler, jobs, services
             )
 
         job_store.get_due_jobs = types.MethodType(
@@ -29,26 +29,26 @@ def inject_dependencies_to_scheduler(scheduler: BaseScheduler, ctx: Container):
 
 
 def _convert_raw_to_easy_maintainable_jobs(
-    scheduler: BaseScheduler, jobs: List[Job], ctx: Services
+        scheduler: BaseScheduler, jobs: List[Job], services: Services
 ) -> List[Job]:
     unsafe_pickling = False
     if isinstance(scheduler, BlockingScheduler):
         unsafe_pickling = True  # pragma: no cover
     if unsafe_pickling:
-        return _make_jobs_shared(jobs, scheduler, ctx)
+        return _make_jobs_shared(jobs, scheduler, services)
     for job in jobs:
         origin_func = ref_to_obj(job.func_ref)
-        job.func = normalize_job_executable(origin_func, ctx)
+        job.func = normalize_job_executable(origin_func, services)
     return jobs
 
 
 def _make_jobs_shared(
-    jobs: List[Job], scheduler: BaseScheduler, ctx: Services
+        jobs: List[Job], scheduler: BaseScheduler, services: Services
 ) -> List[SharedJob]:
     shared_jobs: List[SharedJob] = []
     for job in jobs:
         if isinstance(job, SharedJob):
             jobs.append(job)
             continue
-        shared_jobs.append(SharedJob(scheduler, ctx, **job.__getstate__()))
+        shared_jobs.append(SharedJob(scheduler, services, **job.__getstate__()))
     return shared_jobs
